d5427222939750a712be7a459e2f827a418b9240
albertoval99 <albertovalabad@gmail.com>
2025-03-18 14:00:27 +0100

    todo OK

--- socialMalla/src/main/java/com/cpifppiramide/socialMalla/usuarios/domain/UsuarioRepository.java
+++ socialMalla/src/main/java/com/cpifppiramide/socialMalla/usuarios/domain/UsuarioRepository.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 public interface UsuarioRepository {
     void saveUsuario(Usuario usuario);
     Optional<Usuario> buscarPorNombreUsuario(String nombreUsuario);
-    //gggg
+   
 
 }
